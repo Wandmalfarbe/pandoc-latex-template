50ad594fc57f07cbf3c6a67befb771b5e12c07e7
Wandmalfarbe <Wandmalfarbe@users.noreply.github.com>
2018-09-25 20:49:19 +0200

    Added logo on the custom title page.

--- examples/build.sh
+++ examples/build.sh
@@ -5,7 +5,7 @@ cp "../eisvogel.tex" "../eisvogel.latex"
 pandoc "basic-example/basic-example.md"         -o "basic-example/basic-example.pdf"         --from markdown --template "../eisvogel.latex" --listings
 pdftoppm -r 150 -png "basic-example/basic-example.pdf" > "basic-example/basic-example.png"
 
-pandoc "custom-titlepage/custom-titlepage.md"   -o "custom-titlepage/custom-titlepage.pdf"   --from markdown --template "../eisvogel.latex" --listings
+pandoc "custom-titlepage/custom-titlepage.md"   -o "custom-titlepage/custom-titlepage.pdf"   --from markdown --template "../eisvogel.latex" -V "logo=custom-titlepage/logo-inverted.pdf" --listings
 pdftoppm -r 150 -png "custom-titlepage/custom-titlepage.pdf" > "custom-titlepage/custom-titlepage.png"
 
 pandoc "default-titlepage/default-titlepage.md" -o "default-titlepage/default-titlepage.pdf" --from markdown --template "../eisvogel.latex" --listings
