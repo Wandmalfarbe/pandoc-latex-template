5df6bc5acd8122aba8ac4a995fe0080c3ff4e99f
Wandmalfarbe <Wandmalfarbe@users.noreply.github.com>
2019-03-10 20:56:15 +0100

    Changed the release script to also create a `tar.gz` archive. Renamed the build script for the examples to `build-examples.sh`. Changed the gitignore.

--- tools/release.sh
+++ tools/release.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-zip -r -X --exclude="*.DS_Store*" "Eisvogel-${1}.zip" "examples" "eisvogel.tex" "icon.png" "LICENSE" "README.md"
+rm -rf "dist"
+mkdir "dist"
+zip -r -X --exclude="*.DS_Store*" "dist/Eisvogel-${1}.zip" "examples" "eisvogel.tex" "icon.png" "LICENSE" "README.md"
+tar --exclude="*.DS_Store*" --include="examples" --include="eisvogel.tex" --include="icon.png" --include="LICENSE" --include="README.md" -zcvf "dist/Eisvogel-${1}.tar.gz" *
