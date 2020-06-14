4a2adb0b49fe52650e88c47e8f5f591e54a2b9d3
Wandmalfarbe <Wandmalfarbe@users.noreply.github.com>
2020-06-14 20:15:14 +0200

    Issue #175: Create release assets with and without version number for easy access

--- tools/release.sh
+++ tools/release.sh
@@ -2,5 +2,11 @@
 
 rm -rf "dist"
 mkdir "dist"
+
+# create .zip files
 zip -r -X --exclude="*.DS_Store*" "dist/Eisvogel-${1}.zip" "examples" "eisvogel.tex" "icon.png" "LICENSE" "README.md" "CHANGELOG.md"
+cp "dist/Eisvogel-${1}.zip" "dist/Eisvogel.zip"
+
+# create .tar.gz files
 tar --exclude="*.DS_Store*" --include="examples" --include="eisvogel.tex" --include="icon.png" --include="LICENSE" --include="README.md" --include="CHANGELOG.md" -zcvf "dist/Eisvogel-${1}.tar.gz" *
+cp "dist/Eisvogel-${1}.tar.gz" "dist/Eisvogel.tar.gz"
