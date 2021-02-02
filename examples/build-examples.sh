b52d47aaa5c190cfe00d53d1419cf1326f033c44
Wandmalfarbe <Wandmalfarbe@users.noreply.github.com>
2021-02-02 23:25:19 +0100

    Disable color output in build script

--- examples/build-examples.sh
+++ examples/build-examples.sh
@@ -6,19 +6,6 @@ echo "# building examples"
 echo "##"
 echo ""
 
-# Reset
-Color_Off='\033[0m'       # Text Reset
-
-# Regular Colors
-Black='\033[0;30m'        # Black
-Red='\033[0;31m'          # Red
-Green='\033[0;32m'        # Green
-Yellow='\033[0;33m'       # Yellow
-Blue='\033[0;34m'         # Blue
-Purple='\033[0;35m'       # Purple
-Cyan='\033[0;36m'         # Cyan
-White='\033[0;37m'        # White
-
 containsElement () {
   local e match="$1"
   shift
@@ -35,10 +22,10 @@ for f in *; do
     if [ -d "$f" ]; then
 
     	if containsElement "$f" "${skippedExamples[@]}"; then
-    		echo "${Yellow}skipping '$f'${Color_Off}"
+    		echo "skipping '$f'"
     		echo ""
     	else
-    		echo "${Blue}building '$f'${Color_Off}"
+    		echo "building '$f'"
     		cd "$f"
     		echo "    - running pandoc build script"
         	bash "$PWD/build.sh"
