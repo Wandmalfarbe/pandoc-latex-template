802f03543d718301e5e66f9a44d2f550bb5dfe02
Wandmalfarbe <Wandmalfarbe@users.noreply.github.com>
2025-06-15 15:34:24 +0200

    docs: add examples for unicode and emoji characters

--- examples/build-examples.sh
+++ examples/build-examples.sh
@@ -18,21 +18,26 @@ skippedExamples=("language-chinese" "language-japanese")
 
 # loop all files in the current folder
 for f in *; do
-	# run only for folders
+  # run only for folders
     if [ -d "$f" ]; then
+      if containsElement "$f" "${skippedExamples[@]}"; then
+        echo "skipping '$f'"
+        echo ""
+      else
+        echo "building '$f'"
+        cd "$f"
 
-    	if containsElement "$f" "${skippedExamples[@]}"; then
-    		echo "skipping '$f'"
-    		echo ""
-    	else
-    		echo "building '$f'"
-    		cd "$f"
-    		echo "    - running pandoc build script"
-        	bash "$PWD/build.sh"
-        	echo "    - generating preview"
-        	pdftoppm -r 150 -png "document.pdf" > "preview.png"
-        	echo ""
-        	cd ".."
-    	fi
+        #if [ -f "GenerateMarkdownDocument.java" ]; then
+        #  echo "    - generating markdown document"
+        #  java GenerateMarkdownDocument.java
+        #fi
+
+        echo "    - running pandoc build script"
+          bash "$PWD/build.sh"
+          echo "    - generating preview"
+          pdftoppm -r 150 -png "document.pdf" > "preview.png"
+          echo ""
+          cd ".."
+      fi
     fi
 done
