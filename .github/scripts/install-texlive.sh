33b3c1eeec9a005825000665dd5b8c651f00f3b7
Wandmalfarbe <Wandmalfarbe@users.noreply.github.com>
2025-06-19 22:08:02 +0200

    ci: restructure workflow

--- .github/scripts/install-texlive.sh
+++ .github/scripts/install-texlive.sh
@@ -27,3 +27,17 @@ sudo chown -hR "$(whoami)" "$TEXLIVE_DIR"
 # Basic tlmgr setup
 sudo apt-get update -qq && sudo apt-get install xzdec -y
 tlmgr init-usertree
+
+# install LaTeX packages that are not included via the texlive.profile but are needed for some examples
+
+tlmgr install adjustbox background bidi csquotes footmisc footnotebackref fvextra mdframed pagecolor sourcecodepro sourcesanspro titling upquote xurl hardwrap catchfile
+# trial and error
+tlmgr install letltxmacro zref everypage framed collectbox
+# packages needed for the template
+tlmgr install xecjk filehook unicode-math ucharcat pagecolor babel-german ly1 mweights sourcecodepro sourcesanspro mdframed needspace fvextra footmisc footnotebackref background
+# packages only needed for some examples (that include packages via header-includes)
+tlmgr install awesomebox fontawesome5
+# packages only needed for some examples (example boxes-with-pandoc-latex-environment-and-tcolorbox)
+tlmgr install tcolorbox pgf etoolbox environ trimspaces
+# for the example `watermark`
+tlmgr install draftwatermark
