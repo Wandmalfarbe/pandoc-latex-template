81c48be68ee2ef544109c8dd48f255432a0a9eb6
Wandmalfarbe <Wandmalfarbe@users.noreply.github.com>
2026-02-01 14:38:19 +0100

    ci: debugging install Lua filters

--- .github/scripts/install-lua-filters.sh
+++ .github/scripts/install-lua-filters.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
+echo "XDG_CONFIG_HOME $XDG_CONFIG_HOME"
 BASE_DIR="${XDG_CONFIG_HOME:-$HOME/.local/share}"
+echo "BASE_DIR $BASE_DIR"
 PANDOC_DATA_DIR="$BASE_DIR/pandoc/filters"
+echo "PANDOC_DATA_DIR $PANDOC_DATA_DIR"
 mkdir -p "$PANDOC_DATA_DIR"
 
 curl -fLo "$PANDOC_DATA_DIR/diagram.lua" "https://raw.githubusercontent.com/pandoc-ext/diagram/refs/heads/main/_extensions/diagram/diagram.lua"
